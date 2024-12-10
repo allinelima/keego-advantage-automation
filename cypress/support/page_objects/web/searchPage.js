ff77b1df2b0721b74db30acf4356130d28e14620
Aline Lima <allinelima@gmail.com>
2024-12-09 23:47:44 -0300

    Adicionado  SearchPage

--- cypress/support/page_objects/web/searchPage.js
+++ cypress/support/page_objects/web/searchPage.js
@@ -3,14 +3,25 @@ class SearchPage {
       cy.visit('/search');
     }
   
-    searchForProduct(productName) {
-      cy.get('#search').type(productName);
-      cy.get('#searchButton').click();
-    }
+  searchForProduct() {
+    cy.get('#menuSearch').click();
+    cy.get('#autoComplete')
+      .clear()
+      .type('Laptop')
+      .type('{enter}');
+    cy.get('#searchResultLabel').should('have.text', 'Search result: "laptop"');
+  }
   
-    addProductToCart() {
-      cy.get('.product').first().find('.add-to-cart').click();
-    }
+  addProductToCart() {
+    cy.get('#7').click();
+    cy.get('button[name="save_to_cart"]').click();
+    cy.get('tr[id="product"]').should('have.length', 1); 
+    cy.get('tr[id="product"]').within(() => {
+    cy.contains('HP ENVY - 17T TOUCH LAPTOP').should('be.visible');
+    cy.contains('QTY: 2').should('be.visible');
+    cy.contains('$1,699.98').should('be.visible');
+    });
+  }
   
     goToCheckout() {
       cy.get('#checkoutButton').click();

52d0c30d90f4caa0ae2e70397e7938f708825d42
Aline Lima <allinelima@gmail.com>
2024-12-11 14:19:32 -0300

    Merge Feature/bdd web tests into develop (#4) (#5)
    * refatoração funcionalidades
    
    * Refatoracao fluxo de compra

--- cypress/locators/cartPageLocators.js
+++
@@ -1,8 +0,0 @@
-export const cartPageLocators = {
-    cartLink: '#shoppingCartLink',
-    productlink: 'a > h3.ng-binding',
-    productInCart: 'tr.ng-scope > :nth-child(2) > .roboto-regular',
-    checkoutButton: '#checkOutPopUp',
-    productPrice: '.roboto-bold > .roboto-medium'
-  };
-  
--- cypress/locators/homePageLocators.js
+++
@@ -1,6 +0,0 @@
-export const homePageLocators = {
-    searchButton: '#mobileSearch > .roboto-medium',
-    searchInput: '#mobileSearch > .roboto-medium',
-    searchResult: '.select',
-  };
-  
